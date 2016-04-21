1b2e49391045aea6798b18bc7ff39ec2ddc502b6
Justin Indra Rahardjo <Justin Indra Rahardjo>
2016-04-21 15:07:32 +1000

    updated versions

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'justindra:accounts-tanda',
-  version: '0.0.3',
+  version: '0.0.4',
   summary: 'Login service for Tanda accounts',
   git: 'https://github.com/justindra/meteor-accounts-tanda.git',
   documentation: 'README.md'
@@ -12,7 +12,7 @@ Package.onUse(function(api) {
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('justindra:tanda@0.1.1', ['client', 'server']);
+  api.use('justindra:tanda@0.1.4', ['client', 'server']);
 
   api.addFiles('tanda_login_button.css', 'client');
 
